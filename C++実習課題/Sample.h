01365f2653b40d3ff1df32e44d3ea06b6c2ed060
s_moto <230404@jc-21.jp>
2024-06-11 12:57:12 +0900

    課題１完了

---
+++ C++実習課題/Sample.h
@@ -0,0 +1,18 @@
+/*
+* Sample.h
+* SampleClassクラスを宣言
+*/
+
+//クラスを宣言
+class SampleClass {
+	//メンバ変数
+	int a;
+	int b;
+	int c;
+
+	//メンバ関数
+public:
+	void Input();
+	void Plus();
+	void Disp();
+};
---
+++ C++実習課題/main.cpp
@@ -0,0 +1,13 @@
+#include "Sample.h"
+
+//メイン関数
+int main()
+{
+	//SampleClassクラスのインスタンス（実態）を作る
+	SampleClass a;
+
+	//3つのメンバ関数を呼び出す
+	a.Input();
+	a.Plus();
+	a.Disp();
+}
