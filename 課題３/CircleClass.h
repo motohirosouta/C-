5ebd54b7ed253d85a856e6372591d374f190b120
s_moto <230404@jc-21.jp>
2024-06-17 10:09:47 +0900

    課題3完了

---
+++ 課題３/CircleClass.h
@@ -0,0 +1,12 @@
+class CircleClass 
+{
+	//ƒƒ“ƒo•Ï”
+	float r; //”¼Œa
+	float area; //–ÊÏ
+
+	//ƒƒ“ƒoŠÖ”
+public:
+	void Input();
+	void Calc();
+	void Disp();
+};
---
+++ 課題３/main.cpp
@@ -0,0 +1,11 @@
+#include "CircleClass.h"
+//メイン関数
+int main()
+{
+	//CircleClass クラスのインスタンス（実態）を作る
+	CircleClass a;
+	//３つのメンバ関数を呼び出す
+	a.Input();
+	a.Calc();
+	a.Disp();
+}
